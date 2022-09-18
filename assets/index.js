0561e1cdded3efa2ca098d6f840b39d83349e8bd
Micah <micahculverhouse1@gmail.com>
2022-09-18 17:11:58 -0600

    formatting and setting variables

---
+++ assets/index.js
@@ -0,0 +1,26 @@
+var time = $("#timerCount")
+var introPage = $("#introSection")
+var highScoreLine = $("#highScoresList")
+var startBtn = $("#start_quiz")
+
+var questionsPage = $("#questionQuestions")
+var question = $("#questionId")
+var grade = $("#checkAnswer")
+
+var resultsPage = $("#quizResults")
+var score = $("#finalScore")
+var initials = $("#initial")
+var submitInitials = $("#userInitials")
+
+var highScoresList = $("#highScorePage")
+var back = $("#back")
+var clear = $("#clear")
+var scoreHistory = $("#scoreHistory")
+
+var qI = 0
+var score = 0
+var questionsArr = []
+
+// startBtn.addEventListener(click, startQuiz(){
+//     questionsPage
+// }
